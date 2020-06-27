0b82d94a68b64f3792f55ecc44f79b72d60b18b3
Vagner Cardoso <vagnercardosoweb@gmail.com>
2020-06-27 02:00:06 -0300

    ensure LocalSavePurchases deletes old caches on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,12 +1,21 @@
 interface CacheStore {
+  delete: () => void;
 }
 
 class CacheStoreSpy implements CacheStore {
   public deleteCallsCount: number = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
 class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {
+  }
+
+  async save(): Promise<void> {
+    this.cacheStore.delete();
   }
 }
 
@@ -16,5 +25,14 @@ describe('LocalSavePurchases', () => {
     const savePurchases = new LocalSavePurchases(cacheStore);
 
     expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+
+  test('should delete old cache on sut.save', async () => {
+    const cacheStore = new CacheStoreSpy();
+    const savePurchases = new LocalSavePurchases(cacheStore);
+
+    await savePurchases.save();
+
+    expect(cacheStore.deleteCallsCount).toBe(1);
   });
 });
